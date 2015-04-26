28c314a18ae2a8604ce8433f5397c357b983a3bc
MilesRussell <MilesRussell@wifi-roaming-128-4-104-185.host.udel.edu>
2015-04-26 17:20:53 -0400

    test2

--- src/main/core/src/edu/udel/cisc275_15S/UDevelopers/UDiscover.java
+++ src/main/core/src/edu/udel/cisc275_15S/UDevelopers/UDiscover.java
@@ -9,11 +9,13 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class UDiscover extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Map map;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		map= new Map();
+		
 	}
 
 	@Override
@@ -21,7 +23,8 @@ public class UDiscover extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		this.map.draw(batch);
+		//batch.draw(img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.end();
 	}
 }
