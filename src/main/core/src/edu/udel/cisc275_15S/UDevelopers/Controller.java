f448fb3b54a9d413d94847a7855c475102dc5cfe
jbosworth <jbosworth2@gmail.com>
2015-04-25 23:40:46 -0400

    Controller Class created. Basic skeleton of necessary attributes and functions are displayed in comments. Even this is likely incomplete. Feel free to add to them, and work on creating/integrating functionality.

---
+++ src/main/core/src/edu/udel/cisc275_15S/UDevelopers/Controller.java
@@ -0,0 +1,28 @@
+package edu.udel.cisc275_15S.evaluate;
+
+/* UDiscover's main class.
+ * 
+ */
+public class Controller {
+	/*Any useful attributes?
+	 * Keep track of progress.
+	 */
+	// List of Questions completed (organize by episode)
+	
+	//   -----Flow of the Game-----
+	//Start with title screen
+	
+	//Move to map screen
+	
+	//An episode is chosen
+		//episode id must be generated onClick
+	
+	//GUI takes graphics and episode id in order to present proper text
+		//Mechanism for questioning and evaluating needed
+	
+	//User returns to map (must be notified if episode is passed or just paused)
+	
+	//Game ends when all episodes are complete
+	
+	
+}
