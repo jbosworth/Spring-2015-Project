7b7b26bf8d9d521aa20635692572a1b1658ff31c
jbosworth <jbosworth2@gmail.com>
2015-04-28 23:53:02 -0400

    XMLReader is properly commented and readFile() is updated: clear lists before reading in a new file.

--- src/main/core/src/edu/udel/cisc275_15S/UDevelopers/InputAndEvaluation/XMLReader.java
+++ src/main/core/src/edu/udel/cisc275_15S/UDevelopers/InputAndEvaluation/XMLReader.java
@@ -11,7 +11,10 @@ import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.*;
 import com.badlogic.gdx.utils.XmlReader.Element;
 
+//This class is used by the GUI to read in dialogue and Q&A
 public class XMLReader {
+	//Store questions, answers, and responses in lists to be retrieved by GUI through
+	//the use of special functions.
 	private ArrayList<Question> questions = new ArrayList<Question>();
 	private ArrayList<Answer> answers = new ArrayList<Answer>();
 	private ArrayList<Response> responses = new ArrayList<Response>();
@@ -24,15 +27,16 @@ public class XMLReader {
 		return INSTANCE;
 	}
 	
-	public ArrayList<Question> getQuestions(){
+	//private getters for use by special functions
+	private ArrayList<Question> getQuestions(){
 		return questions;
 	}
 	
-	public ArrayList<Answer> getAnswers(){
+	private ArrayList<Answer> getAnswers(){
 		return answers;
 	}
 	
-	public ArrayList<Response> getResponses(){
+	private ArrayList<Response> getResponses(){
 		return responses;
 	}
 	
@@ -41,6 +45,9 @@ public class XMLReader {
 		return dialogue;
 	}
 	
+	//GUI calls this to get one single, random question from the list
+	//The resulting list is of the form:
+	//Question, Answer, Answer, Answer, Answer
 	public ArrayList getQuestion(){
 		ArrayList<Text> question = new ArrayList<Text>();
 		readFile("Student Health.xml");
@@ -75,20 +82,26 @@ public class XMLReader {
 		return result;
 	}
 	
-	/* Reads from a given XML file. It parses and stores questions and answers
+	/* Reads from a given XML file. It parses and stores questions, answers, responses, and dialogue
 	 * within the XMLReader. @param file should be the name of the input file. For example, 
 	 * "Student_Resources.xml" is what should be passed to readFile().
 	 */
 	public void readFile(String file){
+		//clear lists
+		this.questions.clear();
+		this.answers.clear();
+		this.responses.clear();
+		this.dialogue.clear();
+		
 		XmlReader reader = new XmlReader();
 		Element root = null;
 		try {
 			root = reader.parse(Gdx.files.internal(file));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+		//Retrieve questions
 		Array<Element> questions = root.getChildrenByName("question");
 		for (Element child : questions)
 		{
@@ -101,6 +114,7 @@ public class XMLReader {
 		    this.questions.add(q);
 		}
 		
+		//Retrieve answers
 		Array<Element> answers = root.getChildrenByName("answer");
 		for (Element child : answers)
 		{
@@ -118,6 +132,7 @@ public class XMLReader {
 		    this.answers.add(a);
 		}
 		
+		//Retrieve responses
 		Array<Element> responses = root.getChildrenByName("response");
 		for (Element child : responses){
 			String qid = child.getChildByName("Qid").getAttribute("q_id");
@@ -129,10 +144,12 @@ public class XMLReader {
 		    this.responses.add(r);
 		}
 		
+		//Retrieve dialogue from characters (there are 3 at most)
 		Array<Element> c1 = root.getChildrenByName("c1");
+		String n1 = root.getAttribute("c1");// get name of character
 		for (Element child : c1)
 		{
-		    String text = child.getAttribute("text");
+		    String text = n1 + ": " + child.getAttribute("text");// add name to beginning of text
 		    String n = child.getAttribute("num");
 		    int num = Integer.parseInt(n);
 		    Dialogue d = new Dialogue(text, num);
@@ -140,9 +157,10 @@ public class XMLReader {
 		}
 		
 		Array<Element> c2 = root.getChildrenByName("c2");
+		String n2 = root.getAttribute("c2");// get name of character
 		for (Element child : c2)
 		{
-		    String text = child.getAttribute("text");
+		    String text = n2 + ": " + child.getAttribute("text");// add name to beginning of text
 		    String n = child.getAttribute("num");
 		    int num = Integer.parseInt(n);
 		    Dialogue d = new Dialogue(text, num);
@@ -150,9 +168,10 @@ public class XMLReader {
 		}
 		
 		Array<Element> c3 = root.getChildrenByName("c3");
+		String n3 = root.getAttribute("c3");// get name of character
 		for (Element child : c3)
 		{
-		    String text = child.getAttribute("text");
+		    String text = n3 + ": " + child.getAttribute("text");// add name to beginning of text
 		    String n = child.getAttribute("num");
 		    int num = Integer.parseInt(n);
 		    Dialogue d = new Dialogue(text, num);
@@ -160,6 +179,7 @@ public class XMLReader {
 		}
 	}
 	
+	//Sorts the dialogue in the correct order of characters' speech
 	private void arrange(){
 		ArrayList<Dialogue> temp = new ArrayList<Dialogue>();
 		Dialogue d = new Dialogue("", 0);
@@ -174,7 +194,7 @@ public class XMLReader {
 						found = true;
 					}
 				}else if(j>max && !found){
-					throw new Exception;// dialogue i not found and j out of bounds
+					throw new Exception();// dialogue i not found and j out of bounds
 				}
 				j++;
 			}
